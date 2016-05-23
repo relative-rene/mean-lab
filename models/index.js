0a945a233818b07c181d04346deedac3c932599a
ilterates <iltates@gmail.com>
2016-05-23 16:31:08 -0700

    changed plural to singular naming in models

--- models/index.js
+++ models/index.js
@@ -2,5 +2,4 @@ var mongoose = require("mongoose");
 mongoose.connect( process.env.MONGOLAB_URI ||
                       process.env.MONGOHQ_URL ||
                       "mongodb://localhost/mean-lab" );
-module.exports.Toilet = require("./toilet.js");
-module.exports.Rating = require("./rating.js");
+module.exports.Toilet = require("./product.js");
